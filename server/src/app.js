5d5b137bdf0cd6a8ee4e7565ce92d7a7610e3b45
Silvio Sol <sadwick1989@gmail.com>
2022-02-17 18:03:08 -0500

    Created scripts to optimize dev and prod deployment

--- server/src/app.js
+++ server/src/app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 
@@ -7,7 +8,13 @@ const app = express();
 app.use(cors({
   origin: 'http://localhost:3000'
 }));
+
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
+ 
 app.use(planetsRouter);
+app.use('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+})
 
 module.exports = app;

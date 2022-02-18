58b2bd76aa6d6ea0f8c309e651c404f828e9db9c
Silvio Sol <sadwick1989@gmail.com>
2022-02-18 13:08:04 -0500

    Added an extra layer to retrieve the data from the models

--- server/src/app.js
+++ server/src/app.js
@@ -3,7 +3,8 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 
-const planetsRouter = require('./routes/planets/planets.router')
+const planetsRouter = require('./routes/planets/planets.router');
+const launchesRouter = require('./routes/launches/launches.router');
 
 const app = express();
 app.use(cors({
@@ -15,7 +16,8 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
  
 app.use(planetsRouter);
-app.use('/', (req, res) => {
+app.use(launchesRouter);
+app.use('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 })
 
